47756bbc4d4c13f6915a0d0cab38dc98c740dd9e
Deepraj <deepraj21.bera@gmail.com>
2024-05-31 10:12:49 +0530

    updated upload endpoints

--- Backend/index.js
+++ Backend/index.js
@@ -42,7 +42,7 @@ app.use('/images',express.static('uploads/images'));
 app.post("/upload",upload.single('product'),(req,res)=>{
     res.json({
         success : 1,
-        image_url : `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://canvas-backend-vgcc.onrender.com/images/${req.file.filename}`
     })
 })
 
